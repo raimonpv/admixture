a669ea67d037834e98f6bb0df3ac357a93c85bba
Raimon Padrós Valls <61795054+raimonpv@users.noreply.github.com>
2024-02-25 22:02:23 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Admixture",
+    version="0.0.1",
+    description="CSE 284 Final Project",
+    url="https://github.com/raimonpv/admixture",
+    python_requires=">=3.7",
+    install_requires=[],
+    packages=find_packages(),
+)
